76206b566d2bc79aa98fa15440d3a71aad52be67
enriquecisneros <rcisneros@rickzebradesigns.com>
2024-05-28 12:26:35 -0400

    IQUSWDS-3: updated npm packages, removed starterkit file for dev upkeep reasons.

--- STARTERKIT/existing-theme/fractal.config.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const yaml = require("yaml");
-const { readFileSync } = require("fs");
-const configFile = readFileSync("./iq.tooling.yml", "utf8");
-const config = yaml.parse(configFile);
-module.exports = require(path.resolve(
-  `${config.iqTooling.iq_uswds_path}/fractal.config`
-));
